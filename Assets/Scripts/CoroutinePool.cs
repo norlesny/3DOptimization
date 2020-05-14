4fb2cdb512850fd5feca73ea10a01867d8c26b89
Norbert Lesny <norlesny@gmail.com>
2020-05-14 23:56:40 +0200

    Add a basic CoroutinePool

---
+++ Assets/Scripts/CoroutinePool.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DefaultNamespace
+{
+	public class CoroutinePool
+	{
+		private static CoroutinePoolBehaviour pool;
+
+		private static CoroutinePoolBehaviour Pool
+		{
+			get
+			{
+				if (pool == null)
+				{
+					pool = new GameObject(nameof(CoroutinePool)).AddComponent<CoroutinePoolBehaviour>();
+				}
+
+				return pool;
+			}
+		}
+
+		public static void RunDelayed(UnityAction action)
+		{
+			Pool.StartCoroutine(delayedAction(action));
+		}
+
+		private static IEnumerator delayedAction(UnityAction action)
+		{
+			yield return new WaitForEndOfFrame();
+			action.Invoke();
+		}
+
+		private class CoroutinePoolBehaviour : MonoBehaviour
+		{
+			// Nothing
+		}
+	}
+}
