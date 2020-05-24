3097454a23609a8f21c1ed4a5ecca796d86d05fc
Norbert Lesny <norlesny@gmail.com>
2020-05-24 15:51:08 +0200

    [LFS] Import Viking Village

---
+++ Assets/VikingVillageCleaned/Scripts/CountVerticesTriangles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CountVerticesTriangles : MonoBehaviour {
+
+	[ContextMenu("Count")]
+	private void Count()
+	{
+		List<MeshFilter> meshFilters = Object.FindObjectsOfType<MeshFilter>().ToList();
+		Debug.Log("Vertex count: "+meshFilters.Sum(filter => filter.mesh.vertexCount));
+		Debug.Log(
+			"Triangles count: "+meshFilters.Sum(filter => filter.mesh.triangles.Length / 3));
+	}
+}
